424aa238feaf70e131f5140ef0f08758fbcd654d
Cris Correa <artudvels@gmail.com>
2023-05-01 21:29:16 -0300

    Creando ui con Next.js

---
+++ autotech-ui/src/pages/index.js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Autotech</title>
+        <meta name="description" content="Compra y venta de vehiculos" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.description}>
+          <p>
+            Saque su turno&nbsp;
+          </p>
+        </div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/autotech-logo.jpg"
+            alt="Autotech Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+
+        <div className={styles.grid}>
+          <Link
+            href="turnos/turnos-cliente"
+            className={styles.card}
+          >
+            <h3>Turno &rarr;</h3>
+            <p>
+              Sacar turno.
+            </p>
+          </Link>
+        </div>
+      </main>
+    </>
+  )
+}
---
+++ autotech-ui/src/pages/turnos/turnos-cliente.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Turno() {
+    return (
+        <>
+            <h1>Sacar turno</h1>
+            <h2>
+                <Link href="/">Volver</Link>
+            </h2>
+        </>
+    );
+}

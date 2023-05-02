887365d9a224b2e3f249710d2dd02c3a423ad185
lc-sanchez <csanchez_98@outlook.com>
2023-05-02 12:30:33 -0300

    modificando modelos nuevamente

--- autotech/busquedatecnicos/models.py
+++ autotech/busquedatecnicos/models.py
@@ -1,12 +1,2 @@
 from django.db import models
 
-class Tecnico(models.Model):
-    id_empleado = models.CharField(max_length=4, primary_key=True)
-    dni = models.CharField(max_length=8) 
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    categoria = models.CharField(max_length=1)
-    contrasena = models.CharField(max_length=20)
-
-    def __str__(self) -> str:
-        return self.id_empleado + " | " + self.dni + " | " + self.nombre + " " + self.apellido + " " + self.categoria
--- autotech/busquedatecnicos/views.py
+++ autotech/busquedatecnicos/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
-from models import Tecnico
-from serializers import TecnicoSerializer
 
-class BusquedaTecnicosView(viewsets.ReadOnlyModelViewSet):
-    queryset = Tecnico.objects.all()
-    serializer = TecnicoSerializer
 
-    #filtrado
-    filtro = [filters.SearchFilter]
-    search_fields = ['nombre', 'apellido', 'anio']
+
+

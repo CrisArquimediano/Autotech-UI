beb9540d9c7678f4cc2ec79bedb914ea4d8e5caf
Cris Correa <artudvels@gmail.com>
2023-05-02 20:55:49 -0300

    Cambiando calendario por dayPicker en cliente-calendario.

--- autotech-ui/src/pages/turnos/turnos-cliente-calendario.js
+++ autotech-ui/src/pages/turnos/turnos-cliente-calendario.js
@@ -1,13 +1,67 @@
 import React from "react";
-import Fullcalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
 import Link from 'next/link';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+
+
+const lastMonday = dayjs().startOf('week');
+const nextSunday = dayjs().endOf('week').startOf('day');
+
+const isWeekend = (date) => {
+    const day = date.day();
+
+    return day === 0 || day === 6;
+};
+
+function DateValidationShouldDisableDate() {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer
+                components={['DatePicker', 'DateTimePicker', 'DateRangePicker']}
+            >
+                <DemoItem label="DatePicker">
+                    <DatePicker
+                        defaultValue={nextSunday}
+                        shouldDisableDate={isWeekend}
+                        views={['year', 'month', 'day']}
+                    />
+                </DemoItem>
+            </DemoContainer>
+        </LocalizationProvider>
+    );
+}
 
 function Calendar() {
     return (
         <>
+            <h2>
+                <Link href="./turnos-cliente-talleres">Volver</Link>
+            </h2>
+
+            <div>
+                <DateValidationShouldDisableDate />
+            </div>
+        </>
+    );
+}
+
+export default Calendar;
+
+
+
+
+
+
+
+
+{/*            
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
             <div>
                 <Fullcalendar
                     plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -19,12 +73,4 @@ function Calendar() {
                     }}
                     height={"90vh"}
                 />
-            </div>
-            <h2>
-                <Link href="./turnos-cliente-motivo">Volver</Link>
-            </h2>
-        </>
-    );
-}
-
-export default Calendar;
+            </div> */}
--- autotech-ui/src/pages/turnos/turnos-cliente-motivo.js
+++ autotech-ui/src/pages/turnos/turnos-cliente-motivo.js
@@ -3,8 +3,29 @@ import Link from 'next/link';
 import * as React from 'react';
 import styles from '@/styles/Home.module.css'
 import { Inter } from 'next/font/google'
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 const inter = Inter({ subsets: ['latin'] })
+
+function RadioButtonsGroup() {
+    return (
+        <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Motivo</FormLabel>
+            <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="evaluacion"
+                name="radio-buttons-group"
+            >
+                <FormControlLabel value="service" control={<Radio />} label="Service" />
+                <FormControlLabel value="evaluacion" control={<Radio />} label="Evaluación" />
+            </RadioGroup>
+        </FormControl>
+    );
+}
 
 export default function Motivo() {
     return (
@@ -19,6 +40,10 @@ export default function Motivo() {
                     <p>
                         Elija el motivo de su turno&nbsp;
                     </p>
+                </div>
+
+                <div className={styles.grid}>
+                    <RadioButtonsGroup />
                 </div>
 
                 <div className={styles.grid}>
@@ -40,5 +65,7 @@ export default function Motivo() {
         </>
     );
 }
+
+
 
 

8ec460cd0415902e8abff972ddac02f3a0900879
LuciaSoria <luciacsoria5@gmail.com>
2023-05-06 18:43:01 -0300

    Fix views.py

--- autotech/agenda/views.py
+++ autotech/agenda/views.py
@@ -5,50 +5,46 @@ from administracion.serializers import *
 import requests
 from .gestion_agenda.visualizar_y_modificar_agenda import *
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
 
 class TurnoTallerAPIView(APIView):
-    def get(self, request):
-        taller = request.GET.get("taller_id")
-        products = dias_disponibles_desde_hoy_a_treinta_dias(taller)
-        serializer = TurnoTallerSerializer(products, many=True)
-        return Response(serializer.data)
+    def get(self, request, taller_id:str):
+        products = dias_disponibles_desde_hoy_a_treinta_dias(taller_id)
+        #serializer = TurnoTallerSerializer(products, many=True)
+        #return Response(serializer.data)
+        data = products.json()
+        return JsonResponse(data)
     
-    @csrf_exempt
-    def post(self, request):
-       id_turno = request.get("id_turno")
-       tipo = request.get("tipo")
-       estado = request.get("estado")
-       taller_id = request.get("taller_id")
-       tecnico_id = request.get("tecnico_id")
-       patente = request.get("patente")
-       fecha_inicio = request.get("fecha_inicio")
-       hora_inicio = request.get("hora_inicio")
-       fecha_fin = request.get("fecha_fin")
-       hora_fin = request.get("hora_fin")
-       papeles_en_regla = request.get("papeles_en_regla")
-       
-       #turno = Turno_taller(id_turno=id_turno, tipo=tipo, estado=estado, taller_id=taller_id, tecnico_id=tecnico_id,patente=patente, fecha_inicio=fecha_inicio, hora_inicio=hora_inicio, fecha_fin=fecha_fin, hora_fin=hora_fin, papeles_en_regla=papeles_en_regla)
-       #turno.save()
+    def post(self, request, *args, **kwargs):
+       _id_turno = request.data.get("id_turno")
+       _tipo = request.data.get("tipo")
+       _estado = request.data.get("estado")
+       _taller_id = request.data.get("taller_id")
+       _tecnico_id = request.data.get("tecnico_id")
+       _patente = request.data.get("patente")
+       _fecha_inicio = request.data.get("fecha_inicio")
+       _hora_inicio = request.data.get("hora_inicio")
+       _fecha_fin = request.data.get("fecha_fin")
+       _hora_fin = request.data.get("hora_fin")
+       _papeles_en_regla = request.data.get("papeles_en_regla")
+
        url = 'https://autotech.onrender.com/turnos/'
-       data = { "id_turno": id_turno,
-            "tipo": tipo,
-            "estado": estado,
-            "tecnico_id": tecnico_id,
-            "patente": patente,
-            "fecha_inicio": fecha_inicio,
-            "hora_inicio": hora_inicio,
-            "fecha_fin": fecha_fin,
-            "hora_fin": hora_fin,
-            "papeles_en_regla": papeles_en_regla,
-            "taller_id": taller_id
+       data = { "id_turno": _id_turno,
+            "tipo": _tipo,
+            "estado": _estado,
+            "tecnico_id": _tecnico_id,
+            "patente": _patente,
+            "fecha_inicio": _fecha_inicio,
+            "hora_inicio": _hora_inicio,
+            "fecha_fin": _fecha_fin,
+            "hora_fin": _hora_fin,
+            "papeles_en_regla": _papeles_en_regla,
+            "taller_id": _taller_id
         }
        response = requests.post(url, json=data)
-       
        if response.status_code == 201:
-            # El POST fue exitoso, haz algo con la respuesta si lo necesitas
             print('Turno creado exitosamente')
        else:
-        # El POST falló por alguna razón, maneja el error según corresponda
             print('Hubo un problema al crear el turno')
 
 class TurnoTallerCambioEstadoPapelesAPIView(APIView):

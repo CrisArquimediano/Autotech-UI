e8a124b0161ea15abb3231e3bf2c31257da72016
Joacosan <jacoosanchezz@gmail.com>
2023-05-06 04:43:29 -0300

    completado de tablas

--- autotech/administracion/admin.py
+++ autotech/administracion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class tallerADMIN(admin.ModelAdmin):
+    list_display = ('id_taller', 'nombre', 'id_direccion', 'mail', 'telefono', 'id_sucursal',
+    'capacidad', 'cant_tecnicos')
+
+class turno_tallerADMIN(admin.ModelAdmin):
+    list_display = ('id_turno', 'tipo', 'estado', 'taller_id', 'tecnico_id', 'patente',
+    'fecha_inicio', 'hora_inicio', 'fecha_fin', 'hora_fin', 'papeles_en_regla')
+
+admin.site.register(Taller,tallerADMIN)
+admin.site.register(Turno_taller,turno_tallerADMIN)

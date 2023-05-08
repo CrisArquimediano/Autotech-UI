aa03a65c5dc8e3528fb6789d243dac564a8f0b30
Joacosan <jacoosanchezz@gmail.com>
2023-05-08 13:43:47 -0300

    arreglo requirements.txt y settings

--- autotech/autotech/settings.py
+++ autotech/autotech/settings.py
@@ -43,14 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'administracion',
     'agenda',
-
     'rest_framework',
     'corsheaders',
     'administracion',
     'busquedatecnicos',
-
 ]
 
 MIDDLEWARE = [

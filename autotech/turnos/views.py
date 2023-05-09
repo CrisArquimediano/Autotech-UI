9499286cf95bd62284ed217ea3c073e10bb4d95c
LuciaSoria <luciacsoria5@gmail.com>
2023-05-08 23:16:19 -0300

    Arreglando validaciones en turnos.views.py

--- autotech/turnos/views.py
+++ autotech/turnos/views.py
@@ -4,7 +4,8 @@ from administracion.models import Turno_taller
 from administracion.serializers import TurnoTallerSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from gestion_agenda.visualizar_y_modificar_agenda import *
+from .gestion_agenda.visualizar_y_modificar_agenda import *
+from datetime import *
 
 
 @api_view(['GET'])
@@ -12,6 +13,7 @@ def turnosOverview(request):
     turnos_urls={
         'List':'turnos-list/',
         'Detalle':'turnos-detalle/<str:id_turno>/',
+        'DiasHorariosDisponibles':'horarios-disponibles/<str:taller_id>',
         'Create':'turnos-create/',
         'Update':'turnos-update/<str:id_turno>/',
     }
@@ -44,13 +46,16 @@ def crearTurno(request):
     horario_inicio = request.data.get("hora_inicio")
     horario_fin = request.data.get("hora_fin")
     taller_id = request.data.get("taller_id")
+
+    horario_inicio_time = datetime.strptime(horario_inicio, '%H:%M:%S').time()
+    horario_fin_time = datetime.strptime(horario_fin, '%H:%M:%S').time()
+    dia_time = datetime.strptime(dia, '%Y-%m-%d').date()
     
-    if not horarios_validos(horario_inicio, horario_fin):
+    if not horarios_validos(horario_inicio_time, horario_fin_time):
         return HttpResponse("error: los horarios de comienzo y fin de un turno deben ser horas exactas", status=400)
-    
-    if not esta_disponible(dia, horario_inicio, horario_fin, taller_id):
+    if not esta_disponible(dia_time, horario_inicio_time, horario_fin_time, taller_id):
         return HttpResponse("error: ese dia no esta disponible en ese horario", status=400)
-        
+    
     serializer=TurnoTallerSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -67,5 +72,5 @@ def turnoUpdate(request,id):
     return Response(serializer.data)
 
 def horarios_validos(hora_inicio: time, hora_fin:time):
-    return (hora_inicio.minute != 0 or hora_fin.minute != 0) # and hora_inicio <= hora_fin
+    return hora_inicio.minute == 0 and hora_fin.minute == 0 and hora_inicio.second == 0 and hora_fin.second == 0 # and hora_inicio <= hora_fin
         

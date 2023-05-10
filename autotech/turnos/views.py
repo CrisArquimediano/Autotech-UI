c656c757ec2982eed5dd86c1838a1d96159d916b
LuciaSoria <luciacsoria5@gmail.com>
2023-05-09 23:57:16 -0300

    refactor

--- autotech/turnos/views.py
+++ autotech/turnos/views.py
@@ -43,23 +43,27 @@ def diasHorariosDisponibles(request, taller_id: str):
 @api_view(['POST'])
 def crearTurno(request):
     dia = request.data.get("fecha_inicio")
+    dia_fin = request.data.get("fecha_fin")
     horario_inicio = request.data.get("hora_inicio")
     horario_fin = request.data.get("hora_fin")
     taller_id = request.data.get("taller_id")
 
     horario_inicio_time = datetime.strptime(horario_inicio, '%H:%M:%S').time()
     horario_fin_time = datetime.strptime(horario_fin, '%H:%M:%S').time()
-    dia_date = datetime.strptime(dia, '%Y-%m-%d').date()
+    dia_inicio_date = datetime.strptime(dia, '%Y-%m-%d').date()
+    dia_fin_date = datetime.strptime(dia_fin, '%Y-%m-%d').date()
     
-    if not horarios_validos(horario_inicio_time, horario_fin_time):
+    """if not tiempos_coherentes(horario_inicio_time, horario_fin_time, dia_inicio_date, dia_fin_date):
+        return HttpResponse("error: un turno debe terminar despues de comenzar", status=400)"""
+    if not horarios_exactos(horario_inicio_time, horario_fin_time):
         return HttpResponse("error: los horarios de comienzo y fin de un turno deben ser horas exactas", status=400)
-    if not horarios_dentro_de_rango(dia_date, horario_inicio_time, horario_fin_time):
+    if not horarios_dentro_de_rango(dia_inicio_date, horario_inicio_time, horario_fin_time):
         return HttpResponse("error: los horarios superan el limite de la jornada laboral", status=400)
-    if not dia_valido(dia_date):
+    if not dia_valido(dia_inicio_date):
         return HttpResponse("error: no se puede sacar un turno para una fecha que ya paso.", status=400)
-    if not esta_disponible(dia_date, horario_inicio_time, horario_fin_time, taller_id):
+    if not esta_disponible(dia_inicio_date, horario_inicio_time, horario_fin_time, taller_id):
         return HttpResponse("error: ese dia no esta disponible en ese horario", status=400)
-    
+
     serializer=TurnoTallerSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -75,7 +79,8 @@ def turnoUpdate(request,id):
     
     return Response(serializer.data)
 
-def horarios_validos(hora_inicio:time, hora_fin:time):
+# ------------------- Funciones de validacion -------------------
+def horarios_exactos(hora_inicio:time, hora_fin:time):
     return hora_inicio.minute == 0 and hora_fin.minute == 0 and hora_inicio.second == 0 and hora_fin.second == 0 # and hora_inicio <= hora_fin
         
 def horarios_dentro_de_rango(dia:date, horario_inicio:time, horario_fin:time):
@@ -89,3 +94,11 @@ def horarios_dentro_de_rango(dia:date, horario_inicio:time, horario_fin:time):
     
 def dia_valido(dia: date):
     return dia >= date.today()
+
+"""
+def tiempos_coherentes(horario_inicio: time, horario_fin: time, dia_inicio: date, dia_fin: date):
+    if horario_inicio.hour < horario_fin.hour:
+        return True
+    elif dia_inicio < dia_fin:
+        return True
+"""

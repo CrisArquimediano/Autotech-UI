5249789fe277bfd274e7a8bd559da6ec8f6462ac
Cris <66685602+CrisArquimediano@users.noreply.github.com>
2023-05-12 12:28:16 -0300

    Delete autotech directory
    Limpiando el repo (fork del original) para front

--- autotech/administracion/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from .api import TallerViewSet,TurnoTallerViewSet
-from .api import TallerViewSet,TurnoTallerViewSet
-
-router = routers.DefaultRouter()
-router.register('talleres_admin', TallerViewSet, 'talleres_admin')
-router.register('turnos_admin',TurnoTallerViewSet , 'turnos_admin')
-
-urlpatterns = router.urls
--- autotech/staticfiles/administracion/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import TallerViewSet
-
-router = routers.DefaultRouter()
-router.register('talleres', TallerViewSet, 'talleres')
-
-urlpatterns = router.urls

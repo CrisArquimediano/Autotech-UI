2eba0a4b87b417658b5a7c31bca0cc3dff01cacf
Maite <maitena_escalada@hotmail.com>
2023-05-15 12:41:59 -0300

    modifique tamaño de letras de los titulos de busqueda

--- autotech_ui/src/pages/visualizacion-tecnicos/VisualizacionFiltroBusqueda.jsx
+++ autotech_ui/src/pages/visualizacion-tecnicos/VisualizacionFiltroBusqueda.jsx
@@ -165,7 +165,7 @@ const VisualizacionBusquedaTecnicos = () => {
         <Box className="col-12 col-md-8 col-lg-6 col-xl-6">
           <Box className="card shadow-2-strong" sx={{ borderRadius: "1rem" }}>
             <Box className="card-body p-5 text-center row" elevation={5}>
-              <Typography variant="6" sx={{mb:'10px'}} fontWeight='bold'>Búsqueda:</Typography>
+              <Typography variant="h6" sx={{mb:'10px'}} fontWeight='bold'>Búsqueda:</Typography>
 
               <Input
                 type="search"

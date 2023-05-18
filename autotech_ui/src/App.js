c79bffc4e14b02e75e2189bdf11915ae6513ce23
Cris Correa <artudvels@gmail.com>
2023-05-18 19:41:24 -0300

    Agregando en /asignarTecnico, funcionalidad de Asginar técnico.

--- autotech_ui/src/App.js
+++ autotech_ui/src/App.js
@@ -1,10 +1,11 @@
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
 import TurnoForm from './pages/turnos/turno-cliente/TurnoForm'
 import Home from './pages/home/Home'
 import ControlTecnicos from './pages/visualizacion-tecnicos/ControlTecnicos.jsx';
 import AgendaTaller from './pages/visualizar-agenda/AgendaTaller';
+import AsignacionDeTecnicos from './pages/asignacion-de-tecnico/PanelDeAsignacion'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/turnoForm' element={<TurnoForm />} />
         <Route path='/controlTecnicos' element={<ControlTecnicos />} />
         <Route path='/agendaTaller' element={<AgendaTaller />} />
+        <Route path='/asignarTecnico' element={<AsignacionDeTecnicos />} />
       </Routes>
     </div>
   );
--- autotech_ui/src/index.js
+++ autotech_ui/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-import AsignaciosDeTecnicos from './pages/asignacion-de-tecnico/PanelDeAsignacion';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AsignaciosDeTecnicos />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
